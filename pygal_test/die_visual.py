806548ab5da6f9a0f228c90f6942a4b46034bf45
Homo_sapiens <820043860@qq.com>
2017-12-10 10:26:51 +0800

    模拟掷骰子并绘制直方图

--- pygal_test/die_visual.py
+++ pygal_test/die_visual.py
@@ -1,12 +1,31 @@
 from die import Die
+import pygal
 
 #创建一个D6
 die=Die()
 
 #扔几次骰子，并将结果存储在一个列表中
 results=[]
-for roll_num in range(100):
+for roll_num in range(1000):
     result=die.roll()
     results.append(result)
 
-print(results)
+#分析结果
+frequencies=[]
+for value in range(1,die.number_sides+1):
+    frequency=results.count(value)
+    frequencies.append(frequency)
+
+#对结果进行可视化
+#创建条形图实例
+hist=pygal.Bar()
+
+hist.title="Results of rolling one D6 1000 times."
+hist.x_labels=['1','2','3','4','5','6']
+hist.x_title="Result"
+hist.y_title="Frequency of Result"
+
+hist.add('D6',frequencies)
+hist.render_to_file('die_visual.svg')
+
+print(frequencies)
